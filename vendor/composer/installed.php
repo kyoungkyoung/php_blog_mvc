ab9d84822f397de650347f9a6912def6b827830c
kyoungkyoung <wkjang4@gmail.com>
2021-10-25 21:36:27 +0900

    first commit, 프로젝트 준비

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'bddf4311b4216bab47259215f2ec385983005e9b',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'bddf4311b4216bab47259215f2ec385983005e9b',
+            'dev_requirement' => false,
+        ),
+        'wkjang3/php_framework' => array(
+            'pretty_version' => '1.0.2',
+            'version' => '1.0.2.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../wkjang3/php_framework',
+            'aliases' => array(),
+            'reference' => '8ca8396d84f7c89196c9b2e21a6fe4b04060e229',
+            'dev_requirement' => false,
+        ),
+    ),
+);
